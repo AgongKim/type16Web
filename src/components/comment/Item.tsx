717d4e39567cbcebd2604c88f1b9ba4e5bea9af5
taehyun kim <jamesk3121@gmail.com>
2023-07-26 20:55:05 +0900

    FIX: restore ArguementHot

---
+++ src/components/comment/Item.tsx
@@ -0,0 +1,18 @@
+import {TableCell,TableRow} from "@mui/material";
+import { CommentItem } from "../../types";
+
+export function ArguementCommentItem(props:CommentItem):React.ReactElement {
+    /**
+     * 논쟁 댓글 
+     */
+    return (
+        <TableRow key={props.id}>
+            <TableCell component="th" scope="row">
+                {props.content}
+            </TableCell>
+            <TableCell>{props.user_mbti}</TableCell>
+            <TableCell>{props.like}</TableCell>
+            <TableCell>{props.dislike}</TableCell>
+        </TableRow>
+    );
+}
