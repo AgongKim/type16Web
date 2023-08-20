b878044a3dfac3873338f9be0274986c4c6fd18f
wooram92 <wrjang@mealgen.net>
2023-08-20 22:00:09 +0900

    feat : header, login 페이지 작성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,62 @@
 import React from "react";
 import { PropsWithChildren } from "react";
-import { Link } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {Grid} from "@mui/material";
+import img from "../assets/img";
 
 
 export function Header(props:PropsWithChildren):React.ReactElement {
     /**
      * 논쟁 댓글 리스트
      */
-    const mbti = 'ENTP'
+
+    const location = useLocation();
+    const currentPage = location.pathname;
+
     return (
-        <div className="header">
-            <div className="wrapper horizontal">
-                <div className="tab-container horizontal">
-                    <div className="logo">
-                        TYPE16
-                    </div>
-                    <div className="tab">
-                        <Link to={`/`}>
-                            홈
+        <div style={{alignItems: 'center', paddingBottom: 30, borderBottom: 1, borderBottomStyle: "solid", borderBottomColor: '#d4d4d4'}}>
+            <div style={{width:1140, marginTop: 50,  borderBottomWidth: 1}} className="Header horizontal">
+                <Grid container spacing={1} justifyContent={'flex-start'} alignItems={'flex-end'}>
+                    <Grid item xs={2}>
+                        <Link to='/'>
+                            <img src={img.logo} style={{width:'100%'}} alt="mainLogo"/>
                         </Link>
-                    </div>
-                    <div className="tab">
-                        콜로세움
-                    </div>
-                    <div className="tab">
-                        게시판
-                    </div>
-                    <div className="tab">
-                        <Link to={`/about/${mbti}`}>
-                            평가
+                    </Grid>
+                    <Grid item xs={1} justifyContent={'center'}>
+                        <Link style={headerTitle} to='/'>
+                            <span style={{fontWeight: currentPage === '/' ? 800 : 400}}>홈</span>
                         </Link>
-                    </div>
-                </div>
-                <div className="my-container horizontal">
-                    로그인박스
-                </div>
+                    </Grid>
+                    <Grid item xs={1} justifyContent={'center'}>
+                        <Link style={headerTitle} to='/'>
+                            <span style={{fontWeight: currentPage === '/COLOSSEUM' ? 800 : 400}}>콜로세움</span>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={1} justifyContent={'center'}>
+                        <Link style={headerTitle} to='/'>
+                            <span style={{fontWeight: currentPage === '/BOARD' ? 800 : 400}}>게시판</span>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={1} justifyContent={'center'}>
+                        <Link style={headerTitle} to='/'>
+                            <span style={{fontWeight: currentPage === '/EVALUATION' ? 800 : 400}}>평가</span>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={6} style={{justifyContent: 'flex-end'}}>
+                        <Link style={headerTitle} to='/SignIn'>
+                            <span style={{ padding:10, background: '#1976d2', color: '#fff', fontWeight: 700, fontSize: 14}}>로그인</span>
+                        </Link>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
+}
+
+const headerTitle = {
+    textDecoration: 'none',
+    fontSize: 20,
+    color: '#222',
 }
 
 export default Header
